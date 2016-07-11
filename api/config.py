a5a2a763f68de543bc9de3dbcadacb5eda36985e
dora <dora.korpar@gmail.com>
2016-07-11 10:57:13 -0700

    added josh's ip address

--- api/config.py
+++ api/config.py
@@ -3,11 +3,12 @@ import os
 env = os.environ.get('AIRBNB_ENV')
 
 if env == 'dev':
+	# sets up development environment
 	DEBUG = True
 	HOST = 'localhost'
 	PORT = 3333
 	DATABASE = {
-		'host': ip
+		'host': '158.69.70.185'
 		'user': 'airbnb_user_dev'
 		'database': 'airbnb_dev'
 		'port': 3306
@@ -16,11 +17,12 @@ if env == 'dev':
 	}
 
 if env == 'prod':
+	# sets up production environment
 	DEBUG = False
 	HOST = '0.0.0.0'
 	PORT = 3000
 	DATABASE = {
-		'host': ip 
+		'host': '158.69.70.185'
 		'user': 'airbnb_user_prod'
 		'database': 'airbnb_prod'
 		'port': 3306
