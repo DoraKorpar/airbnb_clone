ca4542ed1bfa5b9691359a7a088e8a029c5a8885
dora <dora.korpar@gmail.com>
2016-07-12 12:20:23 -0700

    added def save to basemodel

---
+++ api/app/models/base.py
@@ -0,0 +1,18 @@
+import peewee
+import datetime
+
+database = peewee.MySQLDatabase()
+
+class BaseModel(peewee.Model):
+
+	id = peewee.PrimaryKeyField(unique=True)
+	created_at = peewee.DateTimeField(default=datetime.datetime.now())
+	updated_at = peewee.DateTimeField(default=datetime.datetime.now())
+
+	def save(self, *args, **kwargs):
+		self.updated_at = datetime.datetime.now()
+		return super(BaseModel, self).save(*args, **kwargs)
+
+	class Meta:
+		database = database
+		order_by = ("id", )
