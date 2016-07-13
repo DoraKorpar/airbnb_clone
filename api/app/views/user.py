328bce0769a31d19a6bbfd9498d8beaf18c97803
dora <dora.korpar@gmail.com>
2016-07-13 13:20:00 -0700

    finished all requests in view user.py

--- api/app/views/user.py
+++ api/app/views/user.py
@@ -7,15 +7,23 @@ from peewee import *
 @app.route("/users", methods=["GET", "POST"])
 def users():
 	if request.method == "POST":
+		# get data from post request
 		data = request.data
 		first_name = data['first_name']
 		last_name = data['last_name']
 		email = data['email']
 		password = data['password']
 
+		# if email already exists in database, return error message
+		email_check = User.get(User.email == email)
+		if email_check:
+			return json_response(code=10000, msg="Email already exists")
+
+		# if email_check is empty, create new user with post request info
 		entry = User.insert(first_name=first_name, last_name=last_name, email=email, password=User.set_password(password))
 		entry.execute()
 
+		# return json of data added to User table
 		user = User.select().where(first_name=first_name, last_name=last_name, email=email).get()
 		return json.dumps(user.to_hash())
 
